84336e544c141481d969fa81da59a417f1976df9
Marcel Wentzien <marcel.wentzien@eah-jena.de>
2023-08-30 12:43:39 +0200

    - [streamlit_app.py] Added session state for metric indicator. Added logo and further text.

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -34,36 +34,38 @@ class RegressionTunableModel(LightningModule):
         return self.encoder(x)
 
 if __name__ == "__main__": 
+    st.image("imgs/eah_logo.jpg", width=400)
     st.title("Predicting the Martensite Start Temperature for Steel Alloys")
-    st.subheader("Details on the used Deep Learning Model can be found in Paper: TODO")
+    st.write("Disclaimer: The results from this tool are estimates based on data consisting of a set of experimental measurements. All results are provided for informational purposes only, in furtherance of the developers' educational mission, to complement the knowledge of materials scientists and engineers, and assist them in their search for new materials with desired properties. The developers may not be held responsible for any decisions based on this tool.")
     full_dataset = StandardScaledDataset()
 
     previous_value = 0
 
     restored_model = RegressionTunableModel.load_from_checkpoint("checkpoint/checkpoint").eval()
 
+    st.write("Input in wt %. Upper limit represents the upper limits within the training data.")
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        c = st.slider('C', 0., 2.25, 0.)
-        mn = st.slider('Mn', 0., 10.24, 0.)
-        si = st.slider('Si', 0., 3.8, 0.)
-        cr = st.slider('Cr', 0., 17.98, 0.)
-        ni = st.slider('Ni', 0., 31.54, 0.)
+        c = st.slider('Carbon (C)', 0., 2.25, 0.)
+        mn = st.slider('Manganese (Mn)', 0., 10.24, 0.)
+        si = st.slider('Silicon (Si)', 0., 3.8, 0.)
+        cr = st.slider('Chromium (Cr)', 0., 17.98, 0.)
+        ni = st.slider('Nickel (Ni)', 0., 31.54, 0.)
 
     with col2:
-        mo = st.slider('Mo', 0., 8.0, 0.)
-        v = st.slider('V', 0., 5.05, 0.)
-        co = st.slider('Co', 0., 16.08, 0.)
-        al = st.slider('Al', 0., 3.01, 0.)
-        w = st.slider('W', 0., 19.2, 0.)
+        mo = st.slider('Molybdenum (Mo)', 0., 8.0, 0.)
+        v = st.slider('Vanadium (V)', 0., 5.05, 0.)
+        co = st.slider('Cobalt (Co)', 0., 16.08, 0.)
+        al = st.slider('Aluminium (Al)', 0., 3.01, 0.)
+        w = st.slider('Tungsten (W)', 0., 19.2, 0.)
 
     with col3:
-        cu = st.slider('Cu', 0., 3.04, 0.)
-        nb = st.slider('Nb', 0., 1.98, 0.)
-        ti = st.slider('Ti', 0., 2.52, 0.)
-        b = st.slider('B', 0., 0.004, 0., 0.001)
-        n = st.slider('N', 0., 2.65, 0.)
+        cu = st.slider('Copper (Cu)', 0., 3.04, 0.)
+        nb = st.slider('Niobium (Nb)', 0., 1.98, 0.)
+        ti = st.slider('Titanium (Ti)', 0., 2.52, 0.)
+        b = st.slider('Boron (B)', 0., 0.004, 0., 0.001)
+        n = st.slider('Nitrogen (N)', 0., 2.65, 0.)
 
     target = pd.DataFrame([c, mn, si, cr, ni, mo, v, co, al, w, cu, nb, ti, b, n], index=full_dataset.data_columns).transpose()
     full_dataset.scale_data_columns(target)
@@ -71,11 +73,20 @@ if __name__ == "__main__":
 
     prediction = restored_model(target)
 
-    rescaled_prediction = full_dataset.rescale_target(prediction).item()
-    rescaled_prediction = f"{rescaled_prediction:10.2f} K"
+    prediction = full_dataset.rescale_target(prediction).item()
+
+    delta = 0
+    if "previous_value" in st.session_state:
+        delta = st.session_state.previous_value - prediction
 
     st.subheader("Martensite Start Temperature:")
-    st.metric("MsT", rescaled_prediction, delta=rescaled_prediction, label_visibility="hidden")
-    previous_value = rescaled_prediction
 
-    st.write("Disclaimer...")
+    _, col4, _ = st.columns(3)
+    with col4:
+        st.metric("MsT", f"{prediction:5.2f} K", delta=delta, label_visibility="hidden")
+
+    st.session_state.previous_value = prediction
+
+    st.subheader("Acknowledgements: ")
+    st.subheader("Details on the used Deep Learning Model can be found in Paper: TODO")
+    st.write("Impressum")
