1132bea4b7b5b3cab12d50fa311ba20188d9e083
Wentzien <marcel.wentzien@eah-jena.de>
2023-09-06 15:55:16 +0200

    - [streamlit_app.py] Allowing alternative input format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,38 +7,40 @@ from src.DeployModel import DeployModel
 def load_model():
     return DeployModel.load_from_checkpoint("src/checkpoint/checkpoint", map_location=torch.device('cpu'))
 
-if __name__ == "__main__": 
-    st.image("imgs/eah_logo.jpg", width=300)
-    st.title("Predicting the Martensite Start Temperature for Steels")
-    st.write("Disclaimer: The developers may not be held responsible for any decisions based on this tool. The model and results are provided for informational and educational purpose only, to assist in the search for new materials with desired properties. The provided tool estimates the results based on publicy available data from a set of experimental measurements.")
-
-    st.write("Input in wt %. Upper limit represents the upper limits within the training data.")
+def define_input(element):
     col1, col2, col3 = st.columns(3)
-    
     with col1:
-        c = st.slider('Carbon (C)', 0., 2.25, 0.)
-        mn = st.slider('Manganese (Mn)', 0., 10.24, 0.)
-        si = st.slider('Silicon (Si)', 0., 3.8, 0.)
-        cr = st.slider('Chromium (Cr)', 0., 17.98, 0.)
-        ni = st.slider('Nickel (Ni)', 0., 31.54, 0.)
+        c = element('Carbon (C)', 0., 2.25, 0.)
+        mn = element('Manganese (Mn)', 0., 10.24, 0.)
+        si = element('Silicon (Si)', 0., 3.8, 0.)
+        cr = element('Chromium (Cr)', 0., 17.98, 0.)
+        ni = element('Nickel (Ni)', 0., 31.54, 0.)
 
     with col2:
-        mo = st.slider('Molybdenum (Mo)', 0., 8.0, 0.)
-        v = st.slider('Vanadium (V)', 0., 5.05, 0.)
-        co = st.slider('Cobalt (Co)', 0., 16.08, 0.)
-        al = st.slider('Aluminium (Al)', 0., 3.01, 0.)
-        w = st.slider('Tungsten (W)', 0., 19.2, 0.)
+        mo = element('Molybdenum (Mo)', 0., 8.0, 0.)
+        v = element('Vanadium (V)', 0., 5.05, 0.)
+        co = element('Cobalt (Co)', 0., 16.08, 0.)
+        al = element('Aluminium (Al)', 0., 3.01, 0.)
+        w = element('Tungsten (W)', 0., 19.2, 0.)
 
     with col3:
-        cu = st.slider('Copper (Cu)', 0., 3.04, 0.)
-        nb = st.slider('Niobium (Nb)', 0., 1.98, 0.)
-        ti = st.slider('Titanium (Ti)', 0., 2.52, 0.)
-        b = st.slider('Boron (B)', 0., 0.004, 0., 0.001, format="%5.3f")
-        n = st.slider('Nitrogen (N)', 0., 2.65, 0.)
-
-    model = load_model()
+        cu = element('Copper (Cu)', 0., 3.04, 0.)
+        nb = element('Niobium (Nb)', 0., 1.98, 0.)
+        ti = element('Titanium (Ti)', 0., 2.52, 0.)
+        b = element('Boron (B)', 0., 0.004, 0., 0.001, format="%5.3f")
+        n = element('Nitrogen (N)', 0., 2.65, 0.)
     prediction = model.inference_vector([c, mn, si, cr, ni, mo, v, co, al, w, cu, nb, ti, b, n])
+    return prediction
 
+def define_sliders():
+    prediction = define_input(st.slider)
+    define_result(prediction, "slider")
+
+def define_numeric():
+    prediction = define_input(st.number_input)
+    define_result(prediction, "numbers")
+
+def define_result(prediction, key):
     st.subheader("Martensite Start Temperature:")
     
     if prediction <= 0:
@@ -46,16 +48,32 @@ if __name__ == "__main__":
     else:
         
         delta = 0
-        if "previous_value" in st.session_state:
-            delta = prediction - st.session_state.previous_value
-
+        if f"previous_value{key}" in st.session_state:
+            delta = prediction - st.session_state[f"previous_value{key}"]
 
         col4, _ = st.columns(2)
         with col4:
             st.metric("MsT", f"{prediction:5.2f} K / {(prediction-273.15):5.2f} °C", delta=f"{delta:5.2f} K/°C", label_visibility="hidden")
             st.write("compared to previous calculation")
 
-        st.session_state.previous_value = prediction
+        st.session_state[f"previous_value{key}"] = prediction
+
+if __name__ == "__main__": 
+    st.image("imgs/eah_logo.jpg", width=300)
+    st.title("Predicting the Martensite Start Temperature for Steels")
+    st.write("Disclaimer: The developers may not be held responsible for any decisions based on this tool. The model and results are provided for informational and educational purpose only, to assist in the search for new materials with desired properties. The provided tool estimates the results based on publicy available data from a set of experimental measurements.")
+
+    st.write("Input in wt %. Upper limit represents the upper limits within the training data.")
+    
+    slider, numbers, others, data = st.tabs(["Slider", "Numbers", "Other models", "Data"])
+
+    model = load_model()
+
+    with slider:
+        define_sliders()
+
+    with numbers:
+        define_numeric()
 
     st.subheader("Acknowledgements: ")
     st.subheader("Details on the used Deep Learning Model can be found in Paper: TODO")
