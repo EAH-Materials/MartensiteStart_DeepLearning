ef40a2e6756814c6e9d1cc055a20d28720604177
Wentzien <marcel.wentzien@eah-jena.de>
2023-09-18 11:49:28 +0200

    - [streamlit_app.py] Adjusted text formatting.

--- streamlit_app.py
+++ streamlit_app.py
@@ -199,6 +199,7 @@ if __name__ == "__main__":
     with data_tab:
         st.dataframe(data, use_container_width=True)
 
-    st.subheader("Acknowledgements: The SteelDesAIn project is funded by the Carl Zeiss Foundation.")
+    st.subheader("Acknowledgements:")
+    st.write("The SteelDesAIn project is funded by the Carl Zeiss Foundation.")
     st.subheader("Details on the used Deep Learning Model can be found in Paper: TODO")
     st.write("Impressum: Work in Progress")
