dab0548c480ebb02ccb921b545975d26f1fafc95
Thomas <thomas.friedrich87@outlook.de>
2023-09-22 18:53:50 +0200

    Improve range_compute, allow model selection

--- demo.py
+++ demo.py
@@ -3,11 +3,11 @@ from src.RangeCompute import range_study
 
 if __name__ == "__main__":
     studies = [
-        {'e1':{"element": "C", "min": 0.0, "max": 1.5, "sample_points": 64}, 'e2':{"element": "Mn", "min": 0.0, "max": 15.0, "sample_points": 64}},
-        {'e1':{"element": "C", "min": 0.0, "max": 1.5, "sample_points": 64}, 'e2':{"element": "Cr", "min": 0.0, "max": 15.0, "sample_points": 64}},
-        {'e1':{"element": "C", "min": 0.0, "max": 1.5, "sample_points": 64}, 'e2':{"element": "V", "min": 0.0, "max": 5.0, "sample_points": 64}},
+        {'e1':{"element": "C", "min": 0.0, "max": 1.5, "sample_points": 16}, 'e2':{"element": "Mn", "min": 0.0, "max": 15.0, "sample_points": 16}},
+        {'e1':{"element": "C", "min": 0.0, "max": 1.5, "sample_points": 16}, 'e2':{"element": "Cr", "min": 0.0, "max": 15.0, "sample_points": 16}},
+        {'e1':{"element": "N", "min": 0.0, "max": 1.5, "sample_points": 16}, 'e2':{"element": "V", "min": 0.0, "max": 5.0, "sample_points": 16}},
                ]
-    fig = range_study(studies)
+    fig = range_study(studies,models=['NN','EM','TD'])
     fig.show()
     fig.write_html("plot.html")
 
--- src/RangeCompute.py
+++ src/RangeCompute.py
@@ -39,7 +39,13 @@ def remove_outlier(array, threshold=5):
     array[array < 0.0] = np.nan
     return array
 
-def range_study(studies, df=None):
+shortcut_to_long = {
+    'EM': 'Empirical (Ingber)',
+    'NN': 'Neural Network',
+    'TD': 'Thermodynamic',
+}
+
+def range_study(studies, models=['NN','EM','TD'], df=None):
     pool = ProcessPoolExecutor(max_workers=cpu_count())
     Ms_ML = DeployModel.load_from_checkpoint("src/checkpoint/checkpoint", map_location="cpu")
 
@@ -51,70 +57,54 @@ def range_study(studies, df=None):
         e2 = study['e2']
         e1_rng = np.linspace(e1["min"], e1["max"], e1["sample_points"])
         e2_rng = np.linspace(e2["min"], e2["max"], e2["sample_points"])
-        Ms_TD = np.zeros([e1["sample_points"], e2["sample_points"]], float)
-        Ms_NN = np.zeros([e1["sample_points"], e2["sample_points"]], float)
-        Ms_EM = np.zeros([e1["sample_points"], e2["sample_points"]], float)
-        futures = []
-        prog = tqdm(None, total=e1["sample_points"] * e2["sample_points"] * 3,desc='Study '+str(sx+1)+'/'+str(len(studies)))
+        Ms = {model: np.zeros([e1["sample_points"], e2["sample_points"]], float) for model in models}
+        futures = {'futures':[],'keys':[]}
+        prog = tqdm(None, total=e1["sample_points"] * e2["sample_points"] * len(models),desc='Study '+str(sx+1)+'/'+str(len(studies)))
         for e1_val in e1_rng:
             for e2_val in e2_rng:
                 composition_dict, composition_vec = comp_dict_vec(e1, e2, e1_val, e2_val, Ms_ML.order)
-                futures.append(pool.submit(Ms_Calphad, **composition_dict))
-                futures.append(pool.submit(Ms_ML.inference_vector, composition_vec))
-                futures.append(pool.submit(Ms_Ingber, **composition_dict))
+                if 'TD' in models:
+                    futures['futures'].append(pool.submit(Ms_Calphad, **composition_dict))
+                    futures['keys'].append('TD')
+                if 'NN' in models:
+                    futures['futures'].append(pool.submit(Ms_ML.inference_vector, composition_vec))
+                    futures['keys'].append('NN')
+                if 'EM' in models:
+                    futures['futures'].append(pool.submit(Ms_Ingber, **composition_dict))
+                    futures['keys'].append('EM')
 
-        for future in as_completed(futures):
+        for _ in as_completed(futures['futures']):
             prog.update(1)
         prog.close()
 
         id = 0
         for e1x in range(len(e1_rng)):
             for e2x in range(len(e2_rng)):
-                Ms_TD[e1x, e2x] = futures[id].result()
-                Ms_NN[e1x, e2x] = futures[id+1].result()
-                Ms_EM[e1x, e2x] = futures[id + 2].result()
-                id += 3
+                for _ in models:
+                    Ms[futures['keys'][id]][e1x, e2x] = futures['futures'][id].result()
+                    id += 1
+        for key in models:
+            Ms[key] = remove_outlier(Ms[key])
 
-        Ms_TD = remove_outlier(Ms_TD)
-        Ms_NN = remove_outlier(Ms_NN)
-        Ms_EM = remove_outlier(Ms_EM)
-
-        measured = import_filter_measured("data/MsDatabase_2022.csv", e1, e2, 3.0, df)
+        measured = import_filter_measured("data/MsDatabase_2022.csv", e1, e2, 0.1, df)
 
         X, Y = np.meshgrid(e1_rng, e2_rng)
-        colors_1 = np.ones(shape=Ms_TD.shape) * 0
-        colors_2 = np.ones(shape=Ms_TD.shape) * 0.5
-        colors_3 = np.ones(shape=Ms_TD.shape)
+        n = len(models)  
+        colors = [np.ones(shape=[e1["sample_points"], e2["sample_points"]]) * (i / max((n - 1),1)) for i in range(n)]
 
-        fig.add_trace(
-            go.Surface(x=X, y=Y, z=Ms_TD.transpose(), name="Thermodynamic", cmin=0, cmax=1, showscale=False, colorscale=colorscale, opacity=0.9, surfacecolor=colors_1),
-            row=1, col=sx+1
-        )
-        fig.add_trace(
-            go.Surface(x=X, y=Y, z=Ms_NN.transpose(), name="Neural Network", cmin=0, cmax=1, colorscale=colorscale, showscale=False, opacity=0.9, surfacecolor=colors_2),
-            row=1, col=sx+1
-        )
-        fig.add_trace(
-            go.Surface(x=X, y=Y, z=Ms_EM.transpose(), name="Empirical", cmin=0, cmax=1, colorscale=colorscale, showscale=False, opacity=0.9, surfacecolor=colors_3),
-            row=1, col=sx+1
-        )
-
-        if sx == 0:
+        for idx, key in enumerate(models):
             fig.add_trace(
-                go.Scatter3d(x=[None], y=[None], z=[None], mode="markers", name="Thermodynamic", showlegend=True, marker=dict(size=36, color=colorscale[0], colorscale=colorscale)),
+                go.Surface(x=X, y=Y, z=Ms[key].transpose(), name=shortcut_to_long[key], cmin=0, cmax=1, showscale=False, colorscale=colorscale, opacity=0.9, surfacecolor=colors[idx],legendgroup='legend_'+str(idx)),
                 row=1, col=sx+1
             )
-            fig.add_trace(
-                go.Scatter3d(x=[None], y=[None], z=[None], mode="markers", name="Neural Network", showlegend=True, marker=dict(size=36, color=colorscale[1], colorscale=colorscale)),
-                row=1, col=sx+1
-            )
-            fig.add_trace(
-                go.Scatter3d(x=[None], y=[None], z=[None], mode="markers", name="Empirical (Ingber)", showlegend=True, marker=dict(size=36, color=colorscale[2], colorscale=colorscale)),
-                row=1, col=sx+1
-            )
+            if sx == 0:
+                fig.add_trace(
+                    go.Scatter3d(x=[None], y=[None], z=[None], mode="markers", name=shortcut_to_long[key], showlegend=True, marker=dict(size=8, color=colorscale[idx], colorscale=colorscale),legendgroup='legend_'+str(idx)),
+                    row=1, col=sx+1
+                )
 
         fig.add_trace(
-            go.Scatter3d(x=measured[e1["element"]], y=measured[e2["element"]], z=measured["Ms"], mode="markers", name="Measured data", showlegend=(sx == 0), marker=dict(size=8, color="black")),
+            go.Scatter3d(x=measured[e1["element"]], y=measured[e2["element"]], z=measured["Ms"], mode="markers", name="Measured data", showlegend=(sx == 0), marker=dict(size=8, color="black"),legendgroup='legend_data'),
             row=1, col=sx+1
         )
 
