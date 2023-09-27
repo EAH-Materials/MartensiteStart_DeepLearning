e4c85bed33221dfe0da6ef32b421210f3207b243
Thomas <thomas.friedrich87@outlook.de>
2023-09-27 18:57:57 +0200

    Add 1D range compute

--- src/RangeCompute.py
+++ src/RangeCompute.py
@@ -13,20 +13,32 @@ from src.MS_Pycalphad import ms_Calphad as Ms_Calphad
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-def import_filter_measured(path, range_1, range_2, threshold=0.05, df=None):
+def import_filter_measured(path, range_1, range_2=None, threshold=0.05, df=None):
     if df is None:
         df = pd.read_csv(path)
     rest_elements = ["C","Mn","Si","Cr","Ni","Mo","V","Co","Al","W","Cu","Nb","Ti","B","N",]
     rest_elements.remove(range_1["element"])
-    rest_elements.remove(range_2["element"])
-    filtered_df = df[
-        (df[range_1["element"]].between(range_1["min"], range_1["max"]))
-        & (df[range_2["element"]].between(range_2["min"], range_2["max"]))
-        & (df[rest_elements] < threshold).all(axis=1)
-    ]
+    if range_2 is not None:
+        rest_elements.remove(range_2["element"])
+        filtered_df = df[
+            (df[range_1["element"]].between(range_1["min"], range_1["max"]))
+            & (df[range_2["element"]].between(range_2["min"], range_2["max"]))
+            & (df[rest_elements] < threshold).all(axis=1)
+        ]
+    else:
+            filtered_df = df[
+            (df[range_1["element"]].between(range_1["min"], range_1["max"]))
+            & (df[rest_elements] < threshold).all(axis=1)
+        ]
     return filtered_df
 
-def comp_dict_vec(e1, e2, e1_val, e2_val, order):
+def comp_dict_vec_1D(e1, e1_val, order):
+    composition_dict = {e1["element"]: e1_val}
+    composition_vec = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    composition_vec[order.index(e1["element"])] = e1_val
+    return composition_dict, composition_vec
+
+def comp_dict_vec_2D(e1, e2, e1_val, e2_val, order):
     composition_dict = {e1["element"]: e1_val, e2["element"]: e2_val}
     composition_vec = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     composition_vec[order.index(e1["element"])] = e1_val
@@ -45,9 +57,76 @@ shortcut_to_long = {
     'TD': 'Thermodynamic',
 }
 
-def range_study(studies, models=['NN','EM','TD'], df=None):
+def range_study_1D(studies, models=['NN','EM','TD'], df=None, Ms_ML=None):
     pool = ProcessPoolExecutor(max_workers=cpu_count())
-    Ms_ML = DeployModel.load_from_checkpoint("src/checkpoint/checkpoint", map_location="cpu")
+    if Ms_ML is None:
+        Ms_ML = DeployModel.load_from_checkpoint("src/checkpoint/checkpoint", map_location="cpu")
+
+    fig = make_subplots(rows=1, cols=len(studies))  
+    colors = ["#ff7f0e", "#1f77b4", "#2ca02c"]
+
+    for sx, study in enumerate(studies):
+        e1 = study['e1']
+        e1_rng = np.linspace(e1["min"], e1["max"], e1["sample_points"])
+        Ms = {model: np.zeros([e1["sample_points"]], float) for model in models}
+        futures = {'futures':[],'keys':[]}
+        prog = tqdm(None, total=e1["sample_points"] * len(models),desc='Study '+str(sx+1)+'/'+str(len(studies)))
+        for e1_val in e1_rng:
+            composition_dict, composition_vec = comp_dict_vec_1D(e1, e1_val, Ms_ML.order)
+            if 'TD' in models:
+                futures['futures'].append(pool.submit(Ms_Calphad, **composition_dict))
+                futures['keys'].append('TD')
+            if 'NN' in models:
+                futures['futures'].append(pool.submit(Ms_ML.inference_vector, composition_vec))
+                futures['keys'].append('NN')
+            if 'EM' in models:
+                futures['futures'].append(pool.submit(Ms_Ingber, **composition_dict))
+                futures['keys'].append('EM')
+
+        for _ in as_completed(futures['futures']):
+            prog.update(1)
+        prog.close()
+
+        id = 0
+        for e1x in range(len(e1_rng)):
+            for _ in models:
+                Ms[futures['keys'][id]][e1x] = futures['futures'][id].result()
+                id += 1
+        for key in models:
+            Ms[key] = remove_outlier(Ms[key])
+
+        measured = import_filter_measured("data/MsDatabase_2022.csv", e1, None, 0.1, df)
+
+        for idx, key in enumerate(models):
+            fig.add_trace(
+                go.Scatter(x=e1_rng, y=Ms[key], name=shortcut_to_long[key], line=dict(color=colors[idx], width=4), showlegend=False, legendgroup='legend_'+str(idx)),
+                row=1, col=sx+1
+            )
+            if sx == 0:
+                fig.add_trace(
+                    go.Scatter(x=[None], y=[None], name=shortcut_to_long[key], line=dict(color=colors[idx], width=4), showlegend=True, legendgroup='legend_'+str(idx)),
+                    row=1, col=sx+1
+                )
+
+            
+        fig.add_trace(
+            go.Scatter(x=measured[e1["element"]], y=measured["Ms"], mode="markers", name="Measured data", showlegend=(sx == 0), marker=dict(size=8, color="black"),legendgroup='legend_data'),
+            row=1, col=sx+1
+        )
+        fig.update_xaxes(title_text=study['e1']["element"] + " [wt%]",row=1, col=sx+1)
+        fig.update_yaxes(title_text="Ms [K]",row=1, col=sx+1)
+
+    fig.update_layout(
+        legend=dict(
+            font=dict(size=30)
+        )
+    )
+    return fig  
+
+def range_study_2D(studies, models=['NN','EM','TD'], df=None, Ms_ML=None):
+    pool = ProcessPoolExecutor(max_workers=cpu_count())
+    if Ms_ML is None:
+        Ms_ML = DeployModel.load_from_checkpoint("src/checkpoint/checkpoint", map_location="cpu")
 
     fig = make_subplots(rows=1, cols=len(studies), specs = [[{'is_3d': True} for _ in range(len(studies))]])  
     colorscale = ["#ff7f0e", "#1f77b4", "#2ca02c"]
@@ -62,7 +141,7 @@ def range_study(studies, models=['NN','EM','TD'], df=None):
         prog = tqdm(None, total=e1["sample_points"] * e2["sample_points"] * len(models),desc='Study '+str(sx+1)+'/'+str(len(studies)))
         for e1_val in e1_rng:
             for e2_val in e2_rng:
-                composition_dict, composition_vec = comp_dict_vec(e1, e2, e1_val, e2_val, Ms_ML.order)
+                composition_dict, composition_vec = comp_dict_vec_2D(e1, e2, e1_val, e2_val, Ms_ML.order)
                 if 'TD' in models:
                     futures['futures'].append(pool.submit(Ms_Calphad, **composition_dict))
                     futures['keys'].append('TD')
@@ -89,7 +168,7 @@ def range_study(studies, models=['NN','EM','TD'], df=None):
         measured = import_filter_measured("data/MsDatabase_2022.csv", e1, e2, 0.1, df)
 
         X, Y = np.meshgrid(e1_rng, e2_rng)
-        n = len(models)  
+        n = len(colorscale)  
         colors = [np.ones(shape=[e1["sample_points"], e2["sample_points"]]) * (i / max((n - 1),1)) for i in range(n)]
 
         for idx, key in enumerate(models):
@@ -99,7 +178,7 @@ def range_study(studies, models=['NN','EM','TD'], df=None):
             )
             if sx == 0:
                 fig.add_trace(
-                    go.Scatter3d(x=[None], y=[None], z=[None], mode="markers", name=shortcut_to_long[key], showlegend=True, marker=dict(size=8, color=colorscale[idx], colorscale=colorscale),legendgroup='legend_'+str(idx)),
+                    go.Scatter3d(x=[None], y=[None], z=[None], mode="markers", name=shortcut_to_long[key], showlegend=True, marker=dict(size=8, color=colorscale[idx]),legendgroup='legend_'+str(idx)),
                     row=1, col=sx+1
                 )
 
@@ -117,11 +196,6 @@ def range_study(studies, models=['NN','EM','TD'], df=None):
 
     fig.update_layout(
         legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=0.0,
-            xanchor="center",
-            x=0.5,
             font=dict(size=30)
         )
     )
