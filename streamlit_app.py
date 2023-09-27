c03cd92fa926d9066813e41c4dd6c0e8fbbe40cb
Thomas <thomas.friedrich87@outlook.de>
2023-09-27 18:58:52 +0200

    Update layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,13 @@ import streamlit as st
 import torch
 import os
 import pandas as pd
-import sklearn
-
-from matplotlib import pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from src.DeployModel import DeployModel
 from src.utilities_steel import Ms_Ingber
 from src.MS_Pycalphad import ms_Calphad
-from src.RangeCompute import range_study
+from src.RangeCompute import range_study_1D, range_study_2D
 from src.lof_ms import LofMs
 
 
@@ -19,15 +18,21 @@ def load_model():
         "src/checkpoint/checkpoint", map_location=torch.device("cpu")
     )
 
+
 @st.cache_resource
 def load_data():
     return pd.DataFrame(pd.read_csv(os.path.join("data", "MsDatabase_2022.csv")))
 
-#@st.cache_resource
-def load_lof():
-    return LofMs(df_data=load_data()) 
+
+# @st.cache_resource
+def load_lof(df_data=None):
+    if df_data is None:
+        df_data = load_data()
+    return LofMs(df_data=df_data)
+
 
 disclaimer = "Limitation of liability for website content: The developers may not be held responsible for any decisions based on this tool. The model and results are provided for informational and educational purpose only, to assist in the search for new materials with desired properties. The provided tool estimates the results based on publicy available data from a set of experimental measurements. The contents were created with the highest standard of applicable care and to the best of the developers knowledge. Nevertheless, the accuracy of the content cannot be guaranteed."
+
 
 def get_inputs(element):
     col1, col2, col3 = st.columns(3)
@@ -72,55 +77,70 @@ def get_inputs(element):
     }
     return vec, dic
 
-def plot_lof_space(lof_space,sample_value):
+
+def plot_lof_space(lof_space, sample_value):
     st.subheader("Data Sample representation:")
-    
-    fig, ax = plt.subplots(1, 2,figsize=(12,3))
-    
-    ax[0].boxplot(lof_space,
-                    vert=False,
-                    #showfliers=False,
-                    flierprops=dict(markerfacecolor='black', marker='x'),
-                    whis=1.5,
-                    patch_artist = True,
-                    boxprops = dict(facecolor = "lightblue")
-        )
-    space = (0.9,1.1)
-    ax[0].set_ylim(space)
-    ax[0].plot([sample_value,sample_value],[space[0],space[1]],'--',color='r',linewidth=0.8)
-    ax[0].axes.get_yaxis().set_visible(False)
-    ax[0].grid()
-    ax[0].set_xlabel('LOF')
 
-    y, _, _ = ax[1].hist(lof_space,color = "black",bins=150, histtype='step')
-    y, _, _ = ax[1].hist(lof_space,color = "lightblue",bins=150)
-    ax[1].plot([sample_value,sample_value],[0,max(y)],'--',color='r',linewidth=0.8)
-    ax[1].grid()
-    ax[1].set_xlabel('LOF')
-    ax[1].set_ylabel('# of samples')
+    fig = make_subplots(rows=1, cols=2, subplot_titles=("Box Plot", "Histogram"))
+    fig.add_trace(
+        go.Box(x=lof_space, name="", showlegend=False, marker_color="#009898"),
+        row=1,
+        col=1,
+    )
+    fig.add_trace(
+        go.Histogram(
+            x=lof_space,
+            nbinsx=150,
+            name="",
+            showlegend=False,
+            marker_color="#009898",
+        ),
+        row=1,
+        col=2,
+    )
+    fig.add_vline(
+        x=sample_value.squeeze(), line_dash="dash", line_color="red", row=1, col="all"
+    )
 
-    st.pyplot(fig,dpi=300)
-    
+    fig.update_layout(
+        xaxis=dict(showgrid=True, title_text="LOF"),
+        xaxis2=dict(showgrid=True, title_text="LOF"),
+        yaxis=dict(showgrid=True),
+        yaxis2=dict(showgrid=True, title_text="# of samples"),
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
 
 def print_result(predictions):
     def __print_func__(value, suffix, head):
         delta = 0
         if f"previous_value_{suffix}" in st.session_state:
             delta = value - st.session_state[f"previous_value_{suffix}"]
-        st.write("**" + head + "**")
-        if value >= 0.0:
+        st.markdown("#### **" + head + "**")
+        if suffix == "LO":
             st.metric(
-                f"MsT_{suffix}",
-                f"{value:5.2f} K / {(value-273.15):5.2f} °C",
-                delta=f"{delta:5.2f} K/°C",
+                f"LOF",
+                f"{predictions[3]:5.2f}",
+                delta=f"{delta:5.2f} ",
                 label_visibility="hidden",
+                delta_color="inverse",
             )
             st.write("compared to previous calculation")
             st.session_state[f"previous_value_{suffix}"] = value
         else:
-            st.write(
-                "The model prediction is very inaccuarate for this alloy (prediction resulted in value below 0 K). Please try a different alloy."
-            )
+            if value >= 0.0:
+                st.metric(
+                    f"MsT_{suffix}",
+                    f"{value:5.2f} K / {(value-273.15):5.2f} °C",
+                    delta=f"{delta:5.2f} K/°C",
+                    label_visibility="hidden",
+                )
+                st.write("compared to previous calculation")
+                st.session_state[f"previous_value_{suffix}"] = value
+            else:
+                st.write(
+                    "The model prediction is very inaccuarate for this alloy (prediction resulted in value below 0 K). Please try a different alloy."
+                )
 
     st.subheader("Martensite Start Temperatures:")
     cols_NN, col_EM, col_TD, col_LO = st.columns(4)
@@ -128,23 +148,21 @@ def print_result(predictions):
         __print_func__(predictions[0], "NN", "Neural Network:")
     with col_EM:
         __print_func__(predictions[1], "EM", "Empirical Model:")
+        with st.expander("ⓘ"):
+            st.markdown(
+                "This [empirical model](https://onlinelibrary.wiley.com/doi/full/10.1002/srin.202100576) estimates the martensite start temperature as a function of chemical composition. It is developed and optimized for high-carbon steels with a Ms range between 0 and 50°C. It is based on the mean value of several empirical models from the literature.",
+                help="$M_{\\text{s}}=\\frac{1}{8}\\left\\{4241.9-2322.27x_{\\text{C}}-284x_{\\text{Mn}}- \n 54.4x_{\\text{Si}}-166.4x_{\\text{Cr}}-137.4x_{\\text{Ni}}-83.5x_{\\text{Mo}}-30x_{\\text{Al}}+38.58x_{\\text{Co}}-600 \\lbrack 1-exp(-0.96x_{\\text{C}}) \\rbrack \\right\\} $",
+            )
     with col_TD:
         __print_func__(predictions[2], "TD", "Thermodynamic Model:")
+        with st.expander("ⓘ"):
+            st.markdown(
+                "This model uses [pyCALPHAD](https://pycalphad.org/) to compute Gibbs energies for Austenite and Martensite over a large temperature range and the [Ghosh-Olson model](https://link.springer.com/article/10.1361/105497101770338653) to compute the driving force for martensitic transformation $Δ_G$."
+            )
+            st.image("imgs/Gibbs_vs_T.png")
     with col_LO:
-        #__print_func__(predictions[3], "LO", "LOF:")
-        delta = 0
-        if f"previous_value_LOF" in st.session_state:
-            delta = predictions[3] - st.session_state[f"previous_value_LOF"]
-        st.write("**" + "Local Outlier Factor:" + "**")
-        st.metric(
-            f"LOF",
-            f"{predictions[3]:5.2f}",
-            delta=f"{delta:5.2f} ",
-            label_visibility="hidden",
-            delta_color="inverse",
-        )
-        st.write("compared to previous calculation")
-        st.session_state[f"previous_value_LOF"] = predictions[3]
+        __print_func__(predictions[3], "LO", "LOF:")
+
 
 if __name__ == "__main__":
     st.set_page_config(
@@ -171,8 +189,17 @@ if __name__ == "__main__":
                     padding-left: 1rem;
                 }
                 .stPlotlyChart {
-                    height: 90vh !important;
+                    height: 80vh !important;
+                }
+                .plot-container {
+                    height: 81vh !important;
                     border: 1px solid #000;
+                }
+                .stDataFrame{
+                    height: 80vh !important;
+                }
+                .stDataFrame > div:nth-child(1){
+                    height: 80vh !important;
                 }
                 #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
@@ -192,90 +219,133 @@ if __name__ == "__main__":
     st.write(
         "Input in wt %. Upper limit represents the upper limits within the training data."
     )
-    comp_tab, range_tab, data_tab = st.tabs(["Calculator", "Range Calculator", "Data"])
+    comp_tab, range_tab, data_tab, info_tab = st.tabs(
+        ["Calculator", "Range Calculator", "Data", "Publication information"]
+    )
 
     model = load_model()
-    data  = load_data()
-    lof   = load_lof()
-    
-    
+    data = load_data()
+    lof = load_lof(data)
+
     with comp_tab:
-        slider_or_numbers = st.toggle("Input with sliders or numeric inputs.",disabled=False)
+        slider_or_numbers = st.toggle(
+            "Input with sliders or numeric inputs.", disabled=False
+        )
         if slider_or_numbers:
             composition_vec, composition_dict = get_inputs(st.slider)
         else:
             composition_vec, composition_dict = get_inputs(st.number_input)
-        
+
         composition_vec_trans = composition_vec.copy()
         Ms_NN = model.inference_vector(composition_vec_trans)
         Ms_EM = Ms_Ingber(**composition_dict)
         Ms_TD = ms_Calphad(**composition_dict, T_guess=Ms_EM)
         Lof_S = lof.score_samples(composition_vec)[0]
-        
-        st.write('\n')
+
+        # st.write("\n")
         print_result([Ms_NN, Ms_EM, Ms_TD, Lof_S])
-        
-        st.write('\n')
-        data_sample_representation = st.toggle("Data Sample representation compared to Dataset.",value=True)
+
+        # st.write("\n")
+        data_sample_representation = st.toggle(
+            "Data Sample representation compared to Dataset.", value=False
+        )
         if data_sample_representation:
-            plot_lof_space(lof_space = lof.dataset_lof(),sample_value=lof.score_samples(composition_vec))
-        
-        
+            plot_lof_space(
+                lof_space=lof.dataset_lof(),
+                sample_value=lof.score_samples(composition_vec),
+            )
+
     with range_tab:
         fig = None
         col1, col2 = st.columns(2)
         with col1:
-            e1 = st.selectbox("Element 1", model.order, 0)
-            e1_lb = st.number_input("Lower Bound 1", 0.0, None, 0.0)
-            e1_ub = st.number_input("Upper Bound 1", 0.0, None, 1.0)
+            st.toggle('Use settings in "Calculator"-tab as reference', False)
+            st.text('Not yet implemented')
         with col2:
-            e2 = st.selectbox("Element 2", model.order, 1)
-            e2_lb = st.number_input("Lower Bound 2", 0.0, None, 0.0)
-            e2_ub = st.number_input("Upper Bound 2", 0.0, None, 1.0)
-        col3, col4, col5, col6 = st.columns(4)
+            dim2d = st.toggle("1D/2D Range", False)
+        if dim2d:
+            col3, col4 = st.columns(2)
+            with col3:
+                e1 = st.selectbox("Element 1", model.order, 0)
+                e1_lb = st.number_input("Lower Bound 1", 0.0, None, 0.0)
+                e1_ub = st.number_input("Upper Bound 1", 0.0, None, 1.0)
+            with col4:
+                e2 = st.selectbox("Element 2", model.order, 1)
+                e2_lb = st.number_input("Lower Bound 2", 0.0, None, 0.0)
+                e2_ub = st.number_input("Upper Bound 2", 0.0, None, 1.0)
+        else:
+            e1 = st.selectbox("Element", model.order, 0)
+            e1_lb = st.number_input("Lower Bound", 0.0, None, 0.0)
+            e1_ub = st.number_input("Upper Bound", 0.0, None, 1.0)
+
+        col5, col6, col7, col8 = st.columns(4)
         models = []
-        with col3:
-            comp = st.button("Compute")
-        with col4:
-            if(st.checkbox('Neural Network',value=True)):
-                models.append('NN')
         with col5:
-            if(st.checkbox('Empirical Model')):
-                models.append('EM')
+            comp = st.button("Compute")
         with col6:
-            if(st.checkbox('Thermodynamic Model')):
-                models.append('TD')
+            if st.checkbox("Neural Network", value=True):
+                models.append("NN")
+        with col7:
+            if st.checkbox("Empirical Model"):
+                models.append("EM")
+        with col8:
+            if st.checkbox("Thermodynamic Model"):
+                models.append("TD")
         if comp:
             fig = None
             with st.spinner("Computing"):
-                study = [
-                    {
-                        "e1": {
-                            "element": e1,
-                            "min": e1_lb,
-                            "max": e1_ub,
-                            "sample_points": 16,
-                        },
-                        "e2": {
-                            "element": e2,
-                            "min": e2_lb,
-                            "max": e2_ub,
-                            "sample_points": 16,
-                        },
-                    }
-                ]
-                fig = range_study(study,df=data,models=models)
+                if dim2d:
+                    study = [
+                        {
+                            "e1": {
+                                "element": e1,
+                                "min": e1_lb,
+                                "max": e1_ub,
+                                "sample_points": 16,
+                            },
+                            "e2": {
+                                "element": e2,
+                                "min": e2_lb,
+                                "max": e2_ub,
+                                "sample_points": 16,
+                            },
+                        }
+                    ]
+                    fig = range_study_2D(study, df=data, models=models, Ms_ML=model)
+                else:
+                    study = [
+                        {
+                            "e1": {
+                                "element": e1,
+                                "min": e1_lb,
+                                "max": e1_ub,
+                                "sample_points": 16,
+                            }
+                        }
+                    ]
+                    fig = range_study_1D(study, df=data, models=models, Ms_ML=model)
         if fig is not None:
             st.plotly_chart(fig, use_container_width=True)
-
 
     with data_tab:
         st.dataframe(data, use_container_width=True)
 
-    st.subheader("Details on the used Deep Learning Model can be found in Paper: TODO")
-    st.subheader("Acknowledgements:")
-    st.write("The SteelDesAIn project is funded by the Carl Zeiss Foundation.")
-    st.markdown("Contains information from TODO: DATABASE NAME, which is made available here under the <a target='_blank' href='https://opendatacommons.org/licenses/odbl/1-0/'>Open Database License (ODbL)</a>.", unsafe_allow_html=True)
-    st.write("This website and the deep learning model are open-source and published under <a target='_blank' href='https://github.com/EAH-Materials/MartensiteStart_DeepLearning/blob/main/LICENSE'>GNU GPLv3</a> on GitHub: <a target='_blank' href='https://github.com/EAH-Materials/MartensiteStart_DeepLearning'>https://github.com/EAH-Materials/MartensiteStart_DeepLearning</a>.", unsafe_allow_html=True)
-    st.write("<a target='_blank' href='https://www.eah-jena.de/impressum'>Impressum</a> (Forwards to the website of the University of Applied Sciences Jena in new tab)", unsafe_allow_html=True)
-    st.write(disclaimer)
+    with info_tab:
+        st.subheader(
+            "Details on the used Deep Learning Model can be found in Paper: TODO"
+        )
+        st.subheader("Acknowledgements:")
+        st.write("The SteelDesAIn project is funded by the Carl Zeiss Foundation.")
+        st.markdown(
+            "Contains information from TODO: DATABASE NAME, which is made available here under the <a target='_blank' href='https://opendatacommons.org/licenses/odbl/1-0/'>Open Database License (ODbL)</a>.",
+            unsafe_allow_html=True,
+        )
+        st.write(
+            "This website and the deep learning model are open-source and published under <a target='_blank' href='https://github.com/EAH-Materials/MartensiteStart_DeepLearning/blob/main/LICENSE'>GNU GPLv3</a> on GitHub: <a target='_blank' href='https://github.com/EAH-Materials/MartensiteStart_DeepLearning'>https://github.com/EAH-Materials/MartensiteStart_DeepLearning</a>.",
+            unsafe_allow_html=True,
+        )
+        st.write(
+            "<a target='_blank' href='https://www.eah-jena.de/impressum'>Impressum</a> (Forwards to the website of the University of Applied Sciences Jena in new tab)",
+            unsafe_allow_html=True,
+        )
+        st.write(disclaimer)
